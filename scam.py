551bfdf1f6ab9cb6116ee9bfacfbc2c726b8e547
Sesath Randira <112878333+SesathRandira@users.noreply.github.com>
2024-11-30 15:32:30 +0530

    Create scam.py

---
+++ scam.py
@@ -0,0 +1,4 @@
+You're Haven't Permission To Open This Code Because It Contain Illigal Things And Scammable Things. Please get permissions by contacting Admin.Try Again Please If You Want It "Nusara-Tech'
+Thank You,We highly recommend to don't open this code.We really sorry for this.
+
+                                                                                This Is An Official Warnning From "GitHub Anti-Malware And Scamming" Team.
